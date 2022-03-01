30eff365f02d69992a75bf47d1f6da6426afb39d
chaksamu <45867824+chaksamu@users.noreply.github.com>
2022-03-01 18:24:21 +0530

    Update Jenkinsfile_exclude_1.groovy

--- Jenkinsfile_exclude_1.groovy
+++ Jenkinsfile_exclude_1.groovy
@@ -46,8 +46,8 @@ pipeline {
                         }
                     }
                     def compileOnPlatforms() {
-
                     //def cDir = new File("./src/com/syniverse/devops/target")
+                        @NonCPS
                         def cDir = new File(".")
                         cDir.eachFileRecurse { file ->
                             if (file.name =~ /.*\.jar$/) {
