870c1103f7488cc417f333153fb212bc15c72a41
chaksamu <45867824+chaksamu@users.noreply.github.com>
2022-03-02 21:07:08 +0530

    Update Jenkinsfile_exclude_1.groovy

--- Jenkinsfile_exclude_1.groovy
+++ Jenkinsfile_exclude_1.groovy
@@ -20,7 +20,7 @@ def FindLog4jversion(i) {
 
 //@NonCPS
 def compileOnPlatforms() {
-	def cDir = new File("./src/com/syniverse/devops/target")
+	def cDir = new File(".")
 	cDir.eachFileRecurse{file ->
 		if (file.name =~ /.*\.jar$/) {
 			println(file)
